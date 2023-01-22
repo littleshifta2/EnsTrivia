0b0fd4acad31d527f142fae4e1cabdae42c62c12
Surafel Aramde <95147827+surrafel@users.noreply.github.com>
2023-01-22 16:01:12 +0100

    Update2

--- questions.js
+++ questions.js
@@ -1,7 +1,9 @@
 
 
-var questions = [{
-
+var questions = [
+    
+    {
+        
     category: "Food", 
     question: "What is the most expensive restaurant in the world?", 
     choices: ["Ithaa Undersea – Maldives", "Aragawa – Tokoyo, Japan", "Guy Savoy – Paris, France", "Sublimotion – Ibiza, Spain"],
@@ -238,6 +240,193 @@ var questions = [{
     
     }, 
     
+    {
+
+    category: "History",
+    question: "Who was the first person to go to space?",
+    choices: ["Neil Armstrong", "John Glenn", "Yuri Gagarin", "Alan Shepard"],
+    correctAnswer: "Yuri Gagarin",
+    imageUrl: "assets/36a.jpg",
+    correctUrl: "assets/36b.jpeg",
+    answer: "Yuri Gagarin was a Soviet Air Force pilot and astronaut who became the first human to journey into outer space on April 12, 1961, when his Vostok spacecraft completed an orbit of the Earth. Gagarin's flight, lasting 108 minutes, was a major achievement in the Cold War and it opened the way for human spaceflight."
+    
+    },
+
+    {
+    category: "History",
+    question: "How many years did the Hundred Years War last?",
+    choices: ["98 years", "160 years", "100 years", "116 years"],
+    correctAnswer: "116 years",
+    imageUrl: "assets/37a.jpg",
+    correctUrl: "assets/37b.jpg",
+    answer: "The Hundred Years War was a series of conflicts fought between England and France from 1337 to 1453. The war lasted 116 years, making it one of the longest and most destructive conflicts in European history. The war was fought primarily over the succession of the French throne and the rights of the English kings to rule parts of France."
+
+    },
+    
+    {
+
+    category: "History",
+    question: "What type of boats did the Vikings use when exploring and raiding?",
+    choices: ["Longboats", "Canoes", "Dinghies", "Galley ships"],
+    correctAnswer: "Longboats",
+    imageUrl: "assets/38a.jpg",
+    correctUrl: "assets/38b.jpg",
+    answer: "The Vikings used longboats, also known as Viking ships, for their exploring and raiding. These boats were narrow and shallow-draft vessels that were designed for speed and maneuverability. They were powered by both sails and oars, and were capable of navigating both deep and shallow waters. They were used for transportation, exploration and warfare and were able to navigate the rough and unpredictable waters of the North Atlantic and the Baltic Sea."
+
+    },
+
+    {
+
+    category: "History",
+    question: "Which country did Nazi Germany invade to kick start World War II?",
+    choices: ["Poland", "France", "Russia", "Britain"],
+    correctAnswer: "Poland",
+    imageUrl: "assets/39a.jpg",
+    correctUrl: "assets/39b.jpg",
+    answer: "On September 1st, 1939, Nazi Germany invaded Poland, which marked the beginning of World War II. The invasion was carried out without a formal declaration of war and it resulted in the rapid defeat of Polish forces. This prompted Britain and France to declare war on Germany, starting the war in Europe."
+
+    },
+
+    {
+
+    category: "History",
+    question: "Which President of the United States abolished slavery?",
+    choices: ["George Washington", "Abraham Lincoln", "Andrew Johnson", "Ulysses S. Grant"],
+    correctAnswer: "Abraham Lincoln",
+    imageUrl: "assets/40a.jpg",
+    correctUrl: "assets/40b.jpg",
+    answer: "Abraham Lincoln, the 16th President of the United States, abolished slavery through the Emancipation Proclamation issued on January 1st, 1863. The proclamation declared all slaves held in Confederate territory to be free, although it did not immediately free a single slave, as it applied only to states that were in rebellion and not under Union control. Lincoln's decision to issue the Emancipation Proclamation was a major step towards ending slavery in the United States and it played a significant role in the outcome of the Civil War."
+
+    },
+
+    {
+
+    category: "Politics",
+    question: "What is the symbol of the Democratic Party in the United States?",
+    choices: ["Donkey", "Elephant", "Rooster", "Bear"],
+    correctAnswer: "Donkey",
+    imageUrl: "assets/41a.png",
+    correctUrl: "assets/41b.jpg",
+    answer: "The symbol of the Democratic Party in the United States is the donkey. The origin of the donkey as a symbol for the Democratic Party dates back to the 1828 presidential campaign of Andrew Jackson, the seventh President of the United States. His opponents referred to him as a 'jackass' for his populist beliefs, but Jackson and his supporters embraced the animal as a symbol of strength and determination. The donkey has been associated with the Democratic Party ever since."
+
+    },
+            
+            
+
+    {
+
+    category: "Politics",
+    question: "Who is the current Secretary-General of the United Nations?",
+    choices: ["Ban Ki-moon", "Kofi Annan", "António Guterres", "Boutros Boutros-Ghali"],
+    correctAnswer: "António Guterres",
+    imageUrl: "assets/42a.jpg",
+    correctUrl: "assets/42b.jpg",
+    answer: "António Guterres is the current Secretary-General of the United Nations. He took office on January 1, 2017. Guterres is a former Prime Minister of Portugal, and served as High Commissioner for Refugees at the United Nations from 2005 to 2015. The Secretary-General is appointed by the General Assembly on the recommendation of the Security Council, and holds the position for a five-year, renewable term."
+
+    },
+
+                
+    {
+
+    category: "Politics",
+    question: "Which country was the first to allow women to vote?",
+    choices: ["United States", "United Kingdom", "New Zealand", "Canada"],
+    correctAnswer: "New Zealand",
+    imageUrl: "assets/43a.jpg",
+    correctUrl: "assets/43b.jpg",
+    answer: "New Zealand was the first country to grant women the right to vote in 1893. The right to vote for women was included in the Electoral Act of 1893, which was passed by the New Zealand Parliament. This made New Zealand the first self-governing country in the world in which all women had the right to vote in parliamentary elections. Women in New Zealand were also able to stand for office and vote in local body elections from the same year."
+
+    },
+    
+                    
+    {
+
+    category: "Politics",
+    question: "Which European city is considered the de facto capital of the European Union?",
+    choices: ["Brussels, Belgium", "Strasbourg, France", "Luxembourg City, Luxembourg", "Berlin, Germany"],
+    correctAnswer: "Brussels, Belgium",
+    imageUrl: "assets/44a.jpg",
+    correctUrl: "assets/44b.png",
+    answer: "Brussels, Belgium, is considered the de facto capital of the European Union. It is home to the European Commission, the Council of the European Union, and the European Council, which are the three main institutions of the European Union. Additionally, Brussels is also where the majority of meetings and negotiations of the EU take place, making it the administrative center of the EU."
+
+    },    
+
+    {
+
+    category: "Politics/Literature",
+    question: "According to George Orwell's book 1984, who is watching us?",
+    choices: ["Big Tech", "Mainstream media", "Big Brother", "Thought police"],
+    correctAnswer: "Big Brother",
+    imageUrl: "assets/45a.jpg",
+    correctUrl: "assets/45b.jpg",
+    answer: "In George Orwell's book 1984, 'Big Brother' is the figurehead leader of the totalitarian government of Oceania, who exerts a powerful control over the population through surveillance and propaganda. The novel depicts a society where the government constantly monitors its citizens through telescreens, thought police, and other means of control, and the phrase 'Big Brother is watching you' has become a popular catchphrase to describe the constant state of surveillance and control. The novel is a portrayal of a dystopian society where the government is constantly monitoring its citizens and suppressing dissent, and the phrase 'Big Brother is watching you' is used to describe the constant state of surveillance and control. "
+
+    },
+            
+
+    {
+
+    category: "Architecture",
+    question: "Where is the tallest building in the world located?",
+    choices: ["Dubai, United Arab Emirates", "Shanghai, China", "New York City, United States", "Taipei, Taiwan"],
+    correctAnswer: "Dubai, United Arab Emirates",
+    imageUrl: "assets/46a.jpg",
+    correctUrl: "assets/46b.jpg",
+    answer: "As of 2021, the tallest building in the world is Burj Khalifa located in Dubai, United Arab Emirates. Burj Khalifa stands at 828 meters (2,722 feet) tall, it opened in 2010 and it held the title of the tallest building in the world for over a decade. It has 163 floors and it is used for residential, office and hotel space. The building was designed by Adrian Smith, of Skidmore, Owings & Merrill, and named after the president of the United Arab Emirates, Khalifa bin Zayed Al Nahyan."
+
+    },
+
+
+
+    {
+
+    category: "Architecture",
+    question: "How many years did it take to build the Taj Mahal?",
+    choices: ["10 years", "20 years", "22 years", "30 years"],
+    correctAnswer: "22 years",
+    imageUrl: "assets/47a.jpg",
+    correctUrl: "assets/47b.jpeg",
+    answer: "The Taj Mahal, a white marble mausoleum located in Agra, India, took 22 years to build. Construction of the Taj Mahal began in 1632 AD, under the direction of the Mughal emperor Shah Jahan, and it was completed in 1653 AD. The Taj Mahal is widely recognized as one of the most beautiful buildings in the world, and is considered a masterpiece of Muslim art and architecture."
+
+    },
+
+    {
+
+    category: "Architecture",
+    question: "The Brandenburg Gate is one of the world's most famous monuments, but in which city would you find it?",
+    choices: ["Paris, France", "Rome, Italy", "Berlin, Germany", "Vienna, Austria"],
+    correctAnswer: "Berlin, Germany",
+    imageUrl: "assets/48a.jpg",
+    correctUrl: "assets/48b.jpg",
+    answer: "The Brandenburg Gate is located in Berlin, Germany. It is considered one of the most famous landmarks in Germany and one of the most important landmarks of European history. It was built in the 18th century and it served as a symbol of peace and unity during the Cold War. The Brandenburg Gate is a UNESCO World Heritage Site and it is a popular tourist destination in Berlin, attracting millions of visitors every year."
+
+    },
+        
+    
+    {
+
+    category: "Architecture",
+    question: "When was the Eiffel Tower built in Paris?",
+    choices: ["1889", "1899", "1909", "1919"],
+    correctAnswer: "1889",
+    imageUrl: "assets/49a.jpg",
+    correctUrl: "assets/49b.jpg",
+    answer: "The Eiffel Tower was built in 1889 in Paris, France. It was constructed as the entrance arch to the 1889 World's Fair, held in Paris to celebrate the 100th anniversary of the French Revolution. The tower is a wrought iron lattice tower and its design was selected by a contest and it was designed and built by the French engineer Gustave Eiffel, who also oversaw its construction. The Eiffel Tower is the most-visited paid monument in the world, and it is considered an iconic symbol of France and one of the most recognizable structures in the world."
+
+    },    
+
+    {
+
+    category: "Architecture",
+    question: "Which famous architect designed the iconic Sydney Opera House?",
+    choices: ["Frank Lloyd Wright", "Le Corbusier", "I.M. Pei", "Jørn Utzon"],
+    correctAnswer: "Jørn Utzon",
+    imageUrl: "assets/50a.jpg",
+    correctUrl: "assets/50b.jpg",
+    answer: "The Sydney Opera House, located in Sydney, Australia, was designed by Danish architect Jørn Utzon. The design of the building was the result of an international competition in 1957, and construction began in 1959. The building was officially opened in 1973, and it is considered one of the most iconic buildings of the 20th century. The Sydney Opera House is a UNESCO World Heritage Site and it's considered one of the most famous architectural landmarks in the world."
+
+    }
+
     
     
     
